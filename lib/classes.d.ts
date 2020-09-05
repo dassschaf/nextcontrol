f981c6195d2e1654892895ef66d039b5b5988bcd
dassschaf <timmy@fruchtwein.net>
2020-09-05 18:01:13 +0200

    more typing

--- lib/classes.d.ts
+++ lib/classes.d.ts
@@ -32,11 +32,33 @@ export class Medals {
     silver  : number;
     gold    : number;
     author  : number;
+
+    /**
+     * Determines what medal the player got
+     * @param {number} time Player's reached time
+     * @returns {string|boolean} Medal name or 'false' when no medal reached
+     */
+    reachedMedal(time : number) : string | boolean;
 }
 
 /**
  * Map Info object
  */
 export class Map {
+    uid : string;
+    name : string;
+    file : string;
+    author : string;
+    envi : string;
+    mood : string;
+    medals : Medals;
+    coppers : number;
+    isMultilap : boolean;
+    nbLaps : number;
+    nbCheckpoints : number;
+    type : string;
+    style : string;
+    tmxid : number;
 
+    setTMXId(id : number) : void;
 }

e43776d474d407b3b245b556a03d61d23635f9ff
dassschaf <timmy@fruchtwein.net>
2020-11-25 15:29:37 +0100

    finishing getID function

--- lib/tmx.d.ts
+++ lib/tmx.d.ts
@@ -2,4 +2,11 @@
 
 export class TMX {
 
+    /**
+     * Returns the TMX ID of a map, based off the map's UID
+     * @param {string} uid map uid
+     * @returns {Number} map's TMX ID *or* -1 if the map is not avaliable at TMX
+     */
+    static async getID(uid) : Number
+
 }

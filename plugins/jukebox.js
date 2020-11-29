f4a4ad5a1c02c90a73e0c28cd23524f9cf6f5a4c
dassschaf <timmy@fruchtwein.net>
2020-11-29 14:48:23 +0100

    perparing jukebox plugin

---
+++ plugins/jukebox.js
@@ -0,0 +1,15 @@
+import { NextControl } from '../nextcontrol.js'
+
+export class JukeboxPlugin {
+    name = 'Jukebox'
+    author = 'dassschaf'
+    description = 'Adds jukebox functionality.'
+
+    /**
+     * 
+     * @param {NextControl} nc 
+     */
+    constructor(nc) {
+        
+    }
+}

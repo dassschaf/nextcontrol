ba452047d23a15c37c86a3eded9f30a4015fa2be
dassschaf <timmy@fruchtwein.net>
2020-12-01 10:43:06 +0100

    implemented jukebox command

--- plugins/jukebox.js
+++ plugins/jukebox.js
@@ -1,6 +1,8 @@
 import { NextControl } from '../nextcontrol.js'
 import { MatchResults } from '../lib/callbackparams.js';
+import { Sentences } from '../lib/sentences.js';
 import * as Classes from '../lib/classes.js';
+import { logger, stripFormatting, isNum } from '../lib/utilities.js';
 
 export class JukeboxPlugin {
     name = 'Jukebox'
@@ -12,6 +14,8 @@ export class JukeboxPlugin {
      * @param {NextControl} nc 
      */
     constructor(nc) {
+        cooldown = new Map();
+
         nc.registerChatCommand(new Classes.ChatCommand('jukebox', this.jukeboxCommand, 'Jukeboxes a map.', this.name));
     }
 
@@ -19,10 +23,42 @@ export class JukeboxPlugin {
      * 
      * @param {String} login 
      * @param {Array<String>} params 
-     * @param {Nextcontrol} nc 
+     * @param {NextControl} nc 
      */
     async jukeboxCommand(login, params, nc) {
+        if (params.length != 1) {
+            await nc.client.query('ChatSendServerMessageToLogin', [Sentences.jukebox.requiresId, login]);
+            return;
+        }
 
+        // check if map query list is empty:
+        if (nc.lists.maps.has(login) && nc.lists.maps.get(login).length > 0) {
+
+            // check if valid number
+            if (isNum(params[0])) {
+                await nc.client.query('ChatSendServerMessageToLogin', [Sentences.jukebox.invalidNumber, login]);
+                return;
+            }
+
+            let id = Number(params[0]);
+
+            // check if valid index
+            if (nc.lists.maps.get(login).length > id - 1) {
+                await nc.client.query('ChatSendServerMessageToLogin', [Sentences.jukebox.invalidIndex, login]);
+                return;
+            }
+            
+            let map = nc.lists.maps.get(login)[id],
+                player = nc.status.getPlayer(login);
+
+            nc.jukebox.queueMap(map, player);
+            logger('r', `Jukebox: ${stripFormatting(player.name)} has successfully jukeboxed track ${stripFormatting(map.name)}.`);
+
+        } else {
+            // tell the user to first query for maps
+            await nc.client.query('ChatSendServerMessageToLogin', [Sentences.jukebox.requiresList, login]);
+            return;
+        }
     }
 
     /**

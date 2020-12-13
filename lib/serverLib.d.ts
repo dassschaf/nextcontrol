331e6cd11599c3be1cb352a576d26c9b7a023288
dassschaf <timmy@fruchtwein.net>
2020-12-13 15:55:41 +0100

    added function library to communicate with the server in a simplified manner

---
+++ lib/serverLib.d.ts
@@ -0,0 +1,51 @@
+/**
+ * Nextcontrol Function library for dedicated server interaction
+ *
+ * Type definition file.
+ */
+import {NextControl} from "../nextcontrol";
+import * as Classes from './classes.js';
+
+
+export class ServerLib {
+    /**
+     * Main object reference
+     * @type {NextControl}
+     */
+    nextcontrol
+
+    /**
+     * Constructs the library object and saves a reference to the main object
+     * @param {NextControl} nextcontrol
+     */
+    constructor(nextcontrol : NextControl)
+
+    /**
+     * Sends a chat message to everyone
+     * @param {String} message Message string
+     * @returns {Promise<void>}
+     */
+    chatMessage(message : String) : Promise<void>
+
+    /**
+     * Sends a chat message to a player given by login
+     * @param {String} login Player login
+     * @param {String} message Message string
+     * @returns {Promise<void>}
+     */
+    chatMessageToLogin(login : String, message : String) : Promise<void>
+
+    /**
+     * Returns the current map info
+     * @returns {Promise<Classes.Map>}
+     */
+    getCurrentMapInfo() : Promise<Classes.Map>
+
+    /**
+     * Returns the map info of a track given by a path
+     * @param {String} path Complete path to file
+     * @returns {Promise<Classes.Map>}
+     */
+    getMapInfo(path : String) : Promise<Classes.Map>
+
+}

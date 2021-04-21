dcf3604db655c703db86979cd7885792d20ee38f
Thomas <timmy@fruchtwein.net>
2021-04-21 14:23:48 +0200

    Adding Database type setting
    Please update your settings files in actual use if you already use this script on a regular server!

--- settings-sample.js
+++ settings-sample.js
@@ -1,24 +1,44 @@
 const Settings = {
-    
+
+    // Trackmania server settings
     trackmania: {
-        port: 5555,           // server port
-        login: 'SuperAdmin',    // login for SuperAdmin rights
-        password: 'SuperAdmin', // password 
 
-        game: 'TM2020',         // game: TM2 or TM2020
+        // the server *has* to run on localhost!
 
-        server_login: 'blah',   // the server's login at the masterserver
+        // server port
+        port: 5555,
 
+        // Authentication details for SuperAdmin access
+        login: 'SuperAdmin',
+        password: 'SuperAdmin',
+
+        // Game: TM2020 or TM2
+        game: 'TM2020',
+
+        // The login of the server at the master server, look it up on the player page in doubt
+        server_login: 'blah',
+
+        // The matchsettings file the server is started with
         matchsettings_file: 'set.txt'
     },
 
+    // Database settings
     database: {
+        // Database server host or URI
         uri: "mongodb://localhost/?poolSize=20&w=majority",
-        database: 'nextcontrol'
+
+        // Database name
+        database: 'nextcontrol',
+
+        // Database type
+        // Supported Databases: 'MongoDB', ... more to come.
+        type: "MongoDB"
     },
 
+    // List of disabled plugins by their name
     disabledPlugins: ['Sample Plugin'],
 
+    // List of administrators by their logins
     admins: [
         'your login here'
     ]

b5472d2e526e7ac0ae92cdb1036b9e940b46ef69
il12 <il5498@yandex.ru>
2021-07-27 19:24:45 +0300

    add error catching to mysql queries, fix mistake with nextcontrol/nc variable in players.js, add temporary version of mysqlCheckTables

---
+++ lib/databases/sql/players.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `players` (
+  `login` varchar(22) NOT NULL COMMENT 'Player login',
+  `name` text NOT NULL COMMENT 'Player name'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
